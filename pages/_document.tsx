74cf68da7a603f537e19aa946dcd4fdbe9606ec6
Maciej Laufer <maciek.laufer@gmail.com>
2022-04-15 09:10:32 +0200

    First configuration

---
+++ pages/_document.tsx
@@ -0,0 +1,17 @@
+import Document, { Head, Html, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="pl">
+        <Head />
+        <body className="bg-gray-100 antialiased">
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
